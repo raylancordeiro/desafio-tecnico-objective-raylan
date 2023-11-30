282628d727f8f6eef17abeca25450c3f25ed6b28
Raylan Cordeiro <raylancordeiro@hotmail.com>
2023-11-30 01:20:19 -0300

    Implementa Testes
    Adiciona classes de teste ao projeto

---
+++ laravel/database/factories/TransacaoFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Transacao;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use \App\Models\Conta;
+
+class TransacaoFactory extends Factory
+{
+    protected $model = Transacao::class;
+
+    public function definition()
+    {
+        return [
+            'conta_id' => Conta::factory(),
+            'valor' => $this->faker->randomFloat(2, 10, 500),
+            'forma_pagamento' => $this->faker->randomElement(['D', 'C', 'P']),
+        ];
+    }
+}
---
+++ laravel/tests/Unit/TransacaoServiceTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Models\Conta;
+use App\Models\Transacao;
+use App\Repositories\ContaRepository;
+use App\Repositories\TransacaoRepository;
+use App\Services\TransacaoService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TransacaoServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_processar_transacao()
+    {
+        $conta = Conta::factory()->create(['saldo' => 1000.00, 'conta_id' => 9999]);
+
+        $transacaoData = [
+            'conta_id' => $conta->conta_id,
+            'valor' => 100.00,
+            'forma_pagamento' => 'D'
+        ];
+
+        $transacaoService = new TransacaoService(new TransacaoRepository(), new ContaRepository());
+
+        $transacao = Transacao::factory()->make($transacaoData)->makeVisible(['id']);
+
+        $transacao = $transacaoService->processarTransacao($transacao->toArray());
+
+        $contaAtualizada = Conta::find($conta->conta_id);
+
+        $valorDebitado = $transacaoData['valor'] * TransacaoService::TAXA[$transacaoData['forma_pagamento']];
+
+        $this->assertEquals($conta->saldo - $valorDebitado, $contaAtualizada->saldo);
+
+        $transacao->delete();
+        $conta->delete();
+
+        $this->assertDatabaseMissing('transacoes', ['id' => $transacao->id]);
+        $this->assertDatabaseMissing('contas', ['conta_id' => $conta->conta_id]);
+    }
+}
